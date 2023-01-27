e80155230808ca2f636d783fc7114cf76da7df4d
Nikhil <nikhilchhillar29@gmail.com>
2023-01-27 21:30:02 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 // navbar
 window.addEventListener("scroll", () => {
     document.querySelector("nav").classList.toggle("window-scroll", window.scrollY > 0)
+});
+
+const faqs = document.querySelectorAll(".faq");
+
+
+faqs.forEach(faq => {
+    faq.addEventListener("click", () => {
+        faq.classList.toggle("open");
+
+        const icon = faq.querySelector(".faq-icon i");
+        if (icon.className === "fa-solid fa-plus") {
+            icon.className = "fa-solid fa-minus";
+        } else {
+            icon.className = "fa-solid fa-plus";
+        }
+    })
 })
